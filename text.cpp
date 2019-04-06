6de531c94e1f576b2bdc771040822fbcfbeea8ea
greedkiss <greedkiss@163.com>
2019-04-06 13:54:21 +0800

    code

--- text.cpp
+++ text.cpp
@@ -1,17 +1,26 @@
 #include<cstdio>
+#include<string>
+#include<vector>
+#include<iostream>
+using namespace std;
+
 int main(){
-    // const char * names[] = {
-    //     "zaea kj",
-    //     "jaj ja",
-    //     "jkm ja"
-    // };
-    // printf("%d\n", names[1]);  
-    // const char* s = "hello wor\0ld !";
-    // printf("%s\n", s);
-    // char array[] = {'a', 'b', 'c', '\0'};
-    // printf("%s\n", array);
-    // printf("%c\n", *(array + 1));
-    char c =  '*';
-    printf("%c", c);
-    return 0;  
+        vector<string> strs = {"", "", ""};
+        int l = strs.size();
+        string str = strs[0];
+        long num = 0, flag = 0;
+        for(long j = 0;;j++){
+           for(long i = 0; i< l-1; i++){
+                if(strs[i][j] != strs[i+1][j]){
+                    flag = 1;
+                    break;
+                }
+            }
+            num++;
+            if(flag)
+                break;
+        }
+        str.resize(num-1);
+        cout<<str<<endl;
+        return 0;
 }
--- undraw.cpp
+++ undraw.cpp
@@ -6,8 +6,6 @@ int  main(int argc,char* argv[])
 {
 	int x= atoi(argv[1]);
 	int y= atoi(argv[3]);
-    // printf("%s\n", argv[0]);
-    printf("%c\n", argv[2]);
 	switch(*argv[2])
 	{
 		case '+':
