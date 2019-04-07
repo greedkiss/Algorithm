472e4026e33c2ab294af1ec2dfd425b6d94cf437
greedkiss <greedkiss@163.com>
2019-04-07 20:29:45 +0800

    submit

--- undraw.cpp
+++ undraw.cpp
@@ -1,31 +1,25 @@
-#include<cstdio>
-#include<cstdlib>
-#include<cstring>
-
-int  main(int argc,char* argv[])
-{
-	int x= atoi(argv[1]);
-	int y= atoi(argv[3]);
-	switch(*argv[2])
-	{
-		case '+':
-			printf( "%d + %d  = %d", x, y, x+y);
-			break;
-		case '-':
-			printf( "%d - %d  = %d", x, y, x-y);
-			break;
-		case '*':
-			printf( "%d * %d  = %d", x, y, x*y); 
-			break;
-		case '/':
-			printf( "%d / %d  = %d", x, y, x/y);
-			break;
-		default:
-			printf( "Correct use: x +(-*/) y ");
-			break;
-		 
-		}
-		getchar();
-        return 0;
-}
-
+class Solution {
+    vector<string> a;
+    void dfs(vector<string>& ans, string& s, int pos, string& digist){
+        if(pos == digist.size()){
+            ans.push_back(s);
+            return;
+        }
+        for(int i =0; i<a[digist[pos]-'0'].size();i++){
+            s += a[pos][i];
+            dfs(ans, s, ++pos, digist);
+            s.pop_back();
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.empty())
+            return ans;
+        a= {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        string s;
+        dfs(ans, s, 0, digits);
+        return ans;
+    }
+    
+};
